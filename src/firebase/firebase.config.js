d5968ccf4a456d9f266b372e9f9ef7b3afff56c5
fayshal69 <foyshalbinamir@gmail.com>
2024-04-08 03:21:27 +0600

    updated firebase key with env vite variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlAvS7f-qI1rv3ySuR8Mnst9KVAMcfTgo",
-  authDomain: "second-home-2f67e.firebaseapp.com",
-  projectId: "second-home-2f67e",
-  storageBucket: "second-home-2f67e.appspot.com",
-  messagingSenderId: "832932187899",
-  appId: "1:832932187899:web:bfe105e27ca8659e8c2a5e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
